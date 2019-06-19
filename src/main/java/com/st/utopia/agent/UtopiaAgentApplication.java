98f48b284030f4810686741b4aa61d43504744cf
Salem <SOzaki19@cornellcollege.edu>
2019-06-19 11:22:58 -0400

    initialization of this project

---
+++ src/main/java/com/st/utopia/agent/UtopiaAgentApplication.java
@@ -0,0 +1,13 @@
+package com.st.utopia.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtopiaAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtopiaAgentApplication.class, args);
+	}
+
+}
