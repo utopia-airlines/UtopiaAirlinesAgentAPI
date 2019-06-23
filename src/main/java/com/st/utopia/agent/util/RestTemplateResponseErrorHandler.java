d1388ebe5dfda1b295233992c3239c920769aca4
Salem <SOzaki19@cornellcollege.edu>
2019-06-23 13:31:41 -0400

    add RestTemplate to application and error handling
    RestTemplate will not throw an error if it receives http 400 or 500 series

---
+++ src/main/java/com/st/utopia/agent/util/RestTemplateResponseErrorHandler.java
@@ -0,0 +1,17 @@
+package com.st.utopia.agent.util;
+
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResponseErrorHandler;
+
+public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
+	@Override
+	public boolean hasError(ClientHttpResponse httpResponse) {
+		/* By default it would return true and throw a error if it was anything other
+		 * than a 200. Now, it will say that anything (including 200-500) is fine, and
+		 * will not throw an error. */
+		return false;
+	}
+	
+	@Override
+	public void handleError(ClientHttpResponse httpResponse) {}
+}
