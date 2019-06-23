8d9e39c27c77e4aff327cdaa96a469b5b3ad2c75
Salem <SOzaki19@cornellcollege.edu>
2019-06-23 13:38:32 -0400

    add comment in restTemplate on the method instead of in the method

--- src/main/java/com/st/utopia/agent/util/RestTemplateResponseErrorHandler.java
+++ src/main/java/com/st/utopia/agent/util/RestTemplateResponseErrorHandler.java
@@ -4,11 +4,12 @@ import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseErrorHandler;
 
 public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
+
+	/* By default it would return true and throw a error if it was anything other
+	 * than a 200. Now, it will say that anything (including 200-500) is fine, and
+	 * will not throw an error. */
 	@Override
 	public boolean hasError(ClientHttpResponse httpResponse) {
-		/* By default it would return true and throw a error if it was anything other
-		 * than a 200. Now, it will say that anything (including 200-500) is fine, and
-		 * will not throw an error. */
 		return false;
 	}
 	
