a2af298090f653413decd7c7a70631895045d3e8
Salem <SOzaki19@cornellcollege.edu>
2019-06-23 13:31:41 -0400

    add RestTemplate to application and error handling
    RestTemplate will not throw an error if it receives http 400 or 500 series

--- src/main/java/com/st/utopia/agent/UtopiaAgentApplication.java
+++ src/main/java/com/st/utopia/agent/UtopiaAgentApplication.java
@@ -2,6 +2,10 @@ package com.st.utopia.agent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.st.utopia.agent.util.RestTemplateResponseErrorHandler;
 
 @SpringBootApplication
 public class UtopiaAgentApplication {
@@ -10,4 +14,10 @@ public class UtopiaAgentApplication {
 		SpringApplication.run(UtopiaAgentApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate getRestTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setErrorHandler(new RestTemplateResponseErrorHandler());
+		return restTemplate;
+	}
 }
