24f680e01b913f5a9d34be0799e7f64637add11f
Salem <SOzaki19@cornellcollege.edu>
2019-06-24 18:58:11 -0400

    remove unused imports

--- src/main/java/com/st/utopia/agent/controller/Flights.java
+++ src/main/java/com/st/utopia/agent/controller/Flights.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
-import com.st.utopia.agent.model.Airport;
 import com.st.utopia.agent.model.Flight;
-import com.st.utopia.agent.model.Ticket;
 
 public class Flights {
 	@Autowired
