f391a3398c90243a65f5aa62c19a9b561939f4a7
Salem <SOzaki19@cornellcollege.edu>
2019-06-24 18:27:32 -0400

    remove non essential code and imports

--- src/main/java/com/st/utopia/agent/controller/Airports.java
+++ src/main/java/com/st/utopia/agent/controller/Airports.java
@@ -3,19 +3,15 @@ package com.st.utopia.agent.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.st.utopia.agent.model.Airport;
 import com.st.utopia.agent.model.Ticket;
-import com.st.utopia.agent.model.User;
 
 /**
  * Controller for agents using this user-facing interface
@@ -30,12 +26,6 @@ public class Airports {
 
 	@Value("${utopia.search.API}")
 	private String searchAPI;
-	
-	@Value("${utopia.booking.API}")
-	private String bookingAPI;
-	
-	@Value("${utopia.cancellation.API}")
-	private String cancellationAPI;
 	
 	/**
 	 * Helper method to reduce the amount of repetitive code required for "get-all"
