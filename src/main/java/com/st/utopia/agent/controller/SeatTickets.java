6459b3fb932b15b42fe83eeaa1830e771eabfbde
al-amine <48740817+al-amine@users.noreply.github.com>
2019-07-09 17:07:43 -0400

    Update SeatTickets.java

--- src/main/java/com/st/utopia/agent/controller/SeatTickets.java
+++ src/main/java/com/st/utopia/agent/controller/SeatTickets.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 
 import com.st.utopia.agent.model.PaymentAmount;
